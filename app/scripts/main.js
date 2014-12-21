37c63e9d4d3ee73033c6a8a8a1232ef68dee8d42
Victoria Nicholson <saintvictoria@gmail.com>
2014-12-20 21:22:36 -0500

    Used regex to strip the protocol from the iamges

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,7 @@ var process = function(data) {
   photos.forEach(function(image) {
     var img = $('<img>');
     var current = image.images.low_resolution;
-    img.attr('src', current.url);
+    img.attr('src', current.url.replace(/^[^:]*:/, ''));
     img.attr('width', current.width);
     img.attr('height', current.height);
     instagram.append(img);
